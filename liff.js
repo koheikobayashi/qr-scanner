33a890dd4b4ab999423a0102c14673b43d65eb42
小林耕平 <>
2021-04-21 15:32:35 +0900

    first commit

---
+++ liff.js
@@ -0,0 +1,29 @@
+window.onload = function() {
+    const defaultLiffId = "xxxxxxxxxxxxx";
+    initializeLiff(defaultLiffId);
+};
+
+function initializeLiff(myLiffId) {
+    liff
+    .init({
+        liffId: myLiffId
+    })
+    .then(() => {
+        liff.scanCode().then(result => {
+            const stringifiedResult = result.value;
+            liff.sendMessages([{
+                'type': 'text',
+                'text': stringifiedResult
+            }]).then(() => {
+                liff.closeWindow();
+            }).catch((error) => {
+                window.alert('Error sending message: ' + error);
+            });
+        }).catch(err => {
+            window.alert('scanCode failed!');
+        });
+    })
+    .catch((err) => {
+        window.alert('Something went wrong with LIFF initialization.');
+    });
+}
